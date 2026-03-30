4afd9696f691bf2ddd7e1b390db2ea06cb3483a2
lucas.valenduc <lucas.valenduc@etu.iut-tlse3.fr>
2026-03-30 14:57:56 +0200

    main.py caca

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # %%
-import numpy as np
-import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [i**2 for i in x]
+print(x, y)
 
 # %%
+import matplotlib.pyplot as plt
+
+plt.plot(x, y)
+plt.title("test")
+plt.show()
